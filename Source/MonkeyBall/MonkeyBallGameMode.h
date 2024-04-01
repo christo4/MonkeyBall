dc3171c6b9fc730403f6b96d9c05c948b8489116
christo4 <christopher.rodrigue@ucalgary.ca>
2024-04-01 13:20:40 -0600

    Initial commit

---
+++ Source/MonkeyBall/MonkeyBallGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MonkeyBallGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMonkeyBallGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMonkeyBallGameMode();
+};
+
+
+
